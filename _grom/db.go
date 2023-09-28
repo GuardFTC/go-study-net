2ca72571b422c78a9247b7daf48cab89eaf33385
fengtiecheng <17615007230@163.com>
2023-09-28 09:59:40 +0800

    feat(gorm): 新增gorm代码
    1.新增gorm代码

---
+++ _grom/db.go
@@ -0,0 +1,33 @@
+// Package _grom @Author:冯铁城 [17615007230@163.com] 2023-09-14 09:22:28
+package _grom
+
+import (
+	"fmt"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+// 数据库连接参数
+var ip = "127.0.0.1"
+var port = "3306"
+var username = "root"
+var password = "root"
+var db = "study"
+
+// InitDB 初始化数据库
+func InitDB() *gorm.DB {
+
+	//1.拼接DSN
+	dsnSuffix := "charset=utf8mb4&parseTime=True"
+	dsn := fmt.Sprintf("%v:%v@tcp(%v:%v)/%v?%v", username, password, ip, port, db, dsnSuffix)
+
+	//2.打开数据库连接
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("database connection error")
+	}
+
+	//3.返回数据库连接
+	return db
+}
--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 // @Author:冯铁城 [17615007230@163.com] 2023-06-21 15:03:33
 package main
 
-import "go-study-net/http_request"
+import (
+	"fmt"
+
+	"go-study-net/_grom"
+)
 
 func main() {
 
@@ -13,6 +17,9 @@ func main() {
 
 	//3.测试HTTP请求
 	testHttpRequest()
+
+	//4.测试Gorm框架
+	testGormDB()
 }
 
 // 测试HTTP服务器
@@ -39,20 +46,45 @@ func testGin() {
 func testHttpRequest() {
 
 	//1.get请求
-	http_request.GetRequest()
+	//http_request.GetRequest()
 
 	//2.post请求
-	http_request.PostRequest()
+	//http_request.PostRequest()
 
 	//3.put请求
-	http_request.PutRequest()
+	//http_request.PutRequest()
 
 	//4.put请求
-	http_request.PatchRequest()
+	//http_request.PatchRequest()
 
 	//5.delete请求
-	http_request.DeleteRequest()
+	//http_request.DeleteRequest()
 
 	//6.上传本地文件
-	http_request.UploadFileFromLocal()
+	//http_request.UploadFileFromLocal()
+}
+
+// 测试Gorm框架
+func testGormDB() {
+
+	//1.初始化数据库连接
+	db := _grom.InitDB()
+
+	//2.创建学生数据库
+	err := db.Set("gorm:table_options", "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='学生表'").AutoMigrate(&_grom.Student{})
+	if err != nil {
+		panic(fmt.Sprintf("create table error:[%v]", err))
+	}
+
+	//3.创建老师数据库
+	err = db.Set("gorm:table_options", "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='老师表'").AutoMigrate(&_grom.Teacher{})
+	if err != nil {
+		panic(fmt.Sprintf("create table error:[%v]", err))
+	}
+
+	//4.创建老师学生中间表数据库
+	err = db.Set("gorm:table_options", "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='老师学生表'").AutoMigrate(&_grom.TeacherAndStudent{})
+	if err != nil {
+		panic(fmt.Sprintf("create table error:[%v]", err))
+	}
 }
